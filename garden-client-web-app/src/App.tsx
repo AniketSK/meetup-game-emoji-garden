ed3c83fe75629d17234b059e4b5558dedb7a87bc
Aniket Kadam <anikadamg@gmail.com>
2019-07-18 12:30:15 +0530

    Setting a reasonable default emoji and changing it when selected.

--- garden-client-web-app/src/App.tsx
+++ garden-client-web-app/src/App.tsx
@@ -4,18 +4,19 @@ import './App.css';
 import CustomEmojiPicker from './CustomEmojiPicker'
 import { Emoji, EmojiData } from 'emoji-mart';
 
-const App: React.FC = () => {
+const defaultEmoji = ":female-technologist::skin-tone-4:"
 
-  const addEmoji = (emojiData : EmojiData) => {
-    console.log(emojiData)
-  }
+const App: React.FC = () => {
+  const [emoji, setEmoji] = React.useState<string>(defaultEmoji)
+
+  const addEmoji = (emojiData : EmojiData) => setEmoji(emojiData.colons || defaultEmoji)
 
   return (
     <div className="App">
 
       <header className="App-header">
         <CustomEmojiPicker addEmoji={addEmoji}/>
-        <div className="App-logo" ><Emoji emoji=':santa::skin-tone-3:' size={64} /></div>
+        <div className="App-logo" ><Emoji emoji={emoji} size={64} /></div>
       </header>
     </div>
   );
