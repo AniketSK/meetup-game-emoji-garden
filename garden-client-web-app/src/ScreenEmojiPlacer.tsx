ed906dd890bf9362897cf89b73ad73411336fe01
Aniket Kadam <anikadamg@gmail.com>
2019-07-18 19:52:34 +0530

    Improved the placeholder for the placing page.

--- garden-client-web-app/src/ScreenEmojiPlacer.tsx
+++ garden-client-web-app/src/ScreenEmojiPlacer.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import AppChrome from './AppChrome'
 
 type ScreenEmojiPlacerTypes = {
     path: string
@@ -6,9 +7,18 @@ type ScreenEmojiPlacerTypes = {
 }
 
 const ScreenEmojiPlacer: React.FC<ScreenEmojiPlacerTypes> = ({emojiId}) => {
+
+
+    const picker : React.FC = () => {
+        return <React.Fragment>Hello, it is I! {decodeURIComponent(emojiId || "yep crash cause of a missing emoji")})</React.Fragment>
+    }
+
+    const submitClicked = () => console.log("Placed")
+
     return (
         <div>
-            Hello, it is I! {decodeURIComponent(emojiId || "yep crash cause of a missing emoji")}
+            <AppChrome screenTitle="Place it in the garden :)" submitClicked={submitClicked} submitText="Place it!"
+            midContent={picker} />
         </div>
     )
 }
