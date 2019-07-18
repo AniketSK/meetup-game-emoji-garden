1c8dfd65b3c5c9dcc4ea4e9efacdd1ac4abb406f
Aniket Kadam <anikadamg@gmail.com>
2019-07-18 11:43:46 +0530

    Rearranged the app display for a better UI

--- garden-client-web-app/src/App.tsx
+++ garden-client-web-app/src/App.tsx
@@ -12,20 +12,10 @@ const App: React.FC = () => {
 
   return (
     <div className="App">
-      <CustomEmojiPicker addEmoji={addEmoji}/>
+
       <header className="App-header">
+        <CustomEmojiPicker addEmoji={addEmoji}/>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
