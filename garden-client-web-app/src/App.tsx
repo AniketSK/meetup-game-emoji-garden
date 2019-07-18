bb55c3736350b0e7eadce1f409230dac658719f1
Aniket Kadam <anikadamg@gmail.com>
2019-07-18 17:31:16 +0530

    Moved the App component into EmojiPickingScreen
    The routes will soon be used to switch the screen into the emoji placing
    screen after the emoji is picked.

--- garden-client-web-app/src/App.tsx
+++ garden-client-web-app/src/App.tsx
@@ -1,28 +1,11 @@
 import React from 'react';
 import './App.css';
-import CustomEmojiPicker from './CustomEmojiPicker'
-import { Emoji, EmojiData } from 'emoji-mart';
-
-const defaultEmoji = ":female-technologist::skin-tone-4:"
+import EmojiPickingScreen from './EmojiPickingScreen'
 
 const App: React.FC = () => {
-  const [emoji, setEmoji] = React.useState<string>(defaultEmoji)
-
-  const showSelectedEmoji = (emojiData : EmojiData) => setEmoji(emojiData.colons || defaultEmoji)
 
   return (
-    <div className="App">
-      <div className="App-title">Pick Your Emoji!</div>
-      <header className="App-header">
-        <CustomEmojiPicker addEmoji={showSelectedEmoji}/>
-        <div className="Emoji-logo">
-          <Emoji emoji={emoji} size={64} />
-        </div>
-      </header>
-      <div className="Button-container">
-        <button className="Button-footer">That's Me!</button>
-      </div>
-    </div>
+    <EmojiPickingScreen />
   );
 }
 
---
+++ garden-client-web-app/src/EmojiPickingScreen.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import './EmojiPickingScreen.css';
+import CustomEmojiPicker from './CustomEmojiPicker'
+import { Emoji, EmojiData } from 'emoji-mart';
+
+const defaultEmoji = ":female-technologist::skin-tone-4:"
+
+const EmojiPickingScreen: React.FC = () => {
+  const [emoji, setEmoji] = React.useState<string>(defaultEmoji)
+
+  const showSelectedEmoji = (emojiData : EmojiData) => setEmoji(emojiData.colons || defaultEmoji)
+
+  return (
+    <div className="App">
+      <div className="App-title">Pick Your Emoji!</div>
+      <header className="App-header">
+        <CustomEmojiPicker addEmoji={showSelectedEmoji}/>
+        <div className="Emoji-logo">
+          <Emoji emoji={emoji} size={64} />
+        </div>
+      </header>
+      <div className="Button-container">
+        <button className="Button-footer">That's Me!</button>
+      </div>
+    </div>
+  );
+}
+
+export default EmojiPickingScreen;
