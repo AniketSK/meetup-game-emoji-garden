8ef0dcb269cb1597508c283673bca29b81c01834
Aniket Kadam <anikadamg@gmail.com>
2019-07-18 13:28:00 +0530

    Refactored a function name to clarify what it does

--- garden-client-web-app/src/App.tsx
+++ garden-client-web-app/src/App.tsx
@@ -8,12 +8,12 @@ const defaultEmoji = ":female-technologist::skin-tone-4:"
 const App: React.FC = () => {
   const [emoji, setEmoji] = React.useState<string>(defaultEmoji)
 
-  const addEmoji = (emojiData : EmojiData) => setEmoji(emojiData.colons || defaultEmoji)
+  const showSelectedEmoji = (emojiData : EmojiData) => setEmoji(emojiData.colons || defaultEmoji)
 
   return (
     <div className="App">
       <header className="App-header">
-        <CustomEmojiPicker addEmoji={addEmoji}/>
+        <CustomEmojiPicker addEmoji={showSelectedEmoji}/>
         <div className="Emoji-logo" ><Emoji emoji={emoji} size={64} /></div>
       </header>
     </div>
